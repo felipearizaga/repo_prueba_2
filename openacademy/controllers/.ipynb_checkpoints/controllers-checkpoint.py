4a6ab64d1d1703b5b18efdd5b6f6876b9c90c183
felipearizaga <44910510+felipearizaga@users.noreply.github.com>
2019-04-05 15:19:29 +0000

    que mierdas es esto

--- openacademy/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ openacademy/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
- class Openacademy(http.Controller):
+class Openacademy(http.Controller):
      @http.route('/openacademy/openacademy/', auth='public')
      def index(self, **kw):
          return "Hello, world"
